cad132c8733462ea1557fecd58272ff672d0f921
azharultambir <70976778+azharultambir@users.noreply.github.com>
2020-11-06 15:48:53 -0500

    Pathfinder header.
    I put everything into the header. Yall want it that way?

--- ProjectAssignment/PathFinder.h
+++ ProjectAssignment/PathFinder.h
@@ -2,10 +2,32 @@
 #include <iostream>
 using namespace std;
 
-class pathfinder
+#include "position.h"
+
+class pathfinder : public position
 {
-  blah
-    
-    
-    
-}
+private:
+	int* map; //pointer to a bi - dimensional array of cell objects.
+	position currentPosition;
+	void display();
+public:
+	void search()
+	{
+		// A path from the initial position to the goal is found.
+		// At the end, the path is displayed.
+		// The path contains the cell positions to follow e.g. (1, 0) -> (1, 1) -> (2, 1) ->
+		// (3, 1).
+		// The path can be displayed from initial position to goal; or from goal to
+		// initial position
+
+
+	}
+	pathfinder(int* pmap, int initialColumn, int initialRow) : currentPosition(initialColumn, initialRow) // CR or RC?
+	{
+		// Argument: Pointer to bi dimensional array(pmap); initialColumn; initialRow.
+		// Initialize map to pmap.
+		// Initialize currentPosition to initialColumn and initialRow
+
+		map = pmap;
+	}
+};
