a2efdfbd760a93398c700f3fb8d9482826a26303
Shuraya <73719278+Shuraya@users.noreply.github.com>
2020-11-06 16:10:57 -0500

    Update Position.h

--- ProjectAssignment/Position.h
+++ ProjectAssignment/Position.h
@@ -1,3 +1,17 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
+class position {
+private:
+	int column;
+	int row;
+public:
+	void setColumn(int acolumn);
+	void setRow(int arow);
+	int getColumn();
+	int getRow();
+	position();
+	position(int arrow, int acolumn);
+
+};
