079d5f24c4c9e3af96ea3cac442d0c7bc5051a9d
azharultambir <70976778+azharultambir@users.noreply.github.com>
2020-12-12 19:18:44 -0500

    position cpp final edit

--- ProjectAssignment/Position.cpp
+++ ProjectAssignment/Position.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include "cell.h"
-#include "pathFinder.h"
 #include "position.h"
-using namespace std;
 
-void position::setColumn(int acolumn) {
+void position::setColumn(int acolumn)
+{
 	column = acolumn;
 }
-void position::setRow(int arow) {
+void position::setRow(int arow)
+{
 	row = arow;
 }
-int position::getColumn() {
+int position::getColumn()
+{
 	return column;
 }
-int position:: getRow() {
+int position::getRow()
+{
 	return row;
 }
-position::position() {
+position::position()
+{
 	row = 0;
 	column = 0;
-
 }
-position::position(int arow, int acolumn) {
-	row = arow;
-	column = acolumn;
-
+position::position(int arow, int acolumn)
+{
+	setRow(arow);
+	setColumn(acolumn);
 }
