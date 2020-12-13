3b36836c6ca15712f53153d955a9e0e1b28c6bcd
azharultambir <70976778+azharultambir@users.noreply.github.com>
2020-12-12 19:17:16 -0500

    pathFinder final edit

--- ProjectAssignment/PathFinder.cpp
+++ ProjectAssignment/PathFinder.cpp
@@ -1,22 +1,25 @@
 #include "pathFinder.h"
-#include "position.h"
 #include <string>
 #include <algorithm>
 
 void pathFinder::search()
 {
-	// call the dfs algorithm
+	int r = currentPosition.getRow();
+	int c = currentPosition.getColumn();
 	vector<string> solution;
-	int r = 2;									// fix this. hard coded
-	int c = 0;
+	// call the dfs algorithm
 	dfs(&currentPosition, r, c, solution);
-	
 	// print the solution
-	reverse(solution.begin(), solution.end());
-	for (size_t i = 0; i < solution.size(); i++)
+	display(solution);
+}
+
+void pathFinder::display(vector<string> s)
+{
+	reverse(s.begin(), s.end());
+	for (size_t i = 0; i < s.size(); i++)
 	{
-		cout << solution[i];
-		if (i < solution.size()-1)
+		cout << s[i];
+		if (i < s.size() - 1)
 			cout << "->";
 	}
 }
@@ -32,10 +35,11 @@ bool pathFinder::dfs(position *p, int r, int c,  vector<string>& v)
 	cell* cellTop = map + ((r-1) * columnSize + c);
 	cell* cellBottom = map + ((r+1) * columnSize + c);
 	
+	string s = "(" + to_string(r) + ',' + to_string(c) + ")";
+
 	// check if this state if the goal (solution)
-	if (currentCell->getCellType() == 2) // is this the goal?
+	if (currentCell->getCellType() == 2)
 	{
-		string s = "(" + to_string(r) + ',' + to_string(c) + ")";
 		v.push_back(s);
 		return true;
 	}
@@ -43,31 +47,25 @@ bool pathFinder::dfs(position *p, int r, int c,  vector<string>& v)
 	// mark this state as visited
 	currentCell->setCellType(3);
 
-	int test = currentCell->getCellType();
-
 	// perform actions *when possible*
-	int cellTypeRight = cellRight->getCellType(); // look over this
+	int cellTypeRight = cellRight->getCellType();
 	int cellTypeLeft = cellLeft->getCellType();
 	int cellTypeTop = cellTop->getCellType();
 	int cellTypeBottom = cellBottom->getCellType();
 
 	if (c < 5 && cellTypeRight != 1 && cellTypeRight != 3 && dfs(p, r, c+1, v)) { // move right
-		string s = "(" + to_string(r) + ',' + to_string(c) + ")";
 		v.push_back(s);
 		return true;
 	}
 	else if (c > 0 && cellTypeLeft != 1 && cellTypeLeft != 3 && dfs(p, r, c-1, v)) { // move left
-		string s = "(" + to_string(r) + ',' + to_string(c) + ")";
 		v.push_back(s);
 		return true;
 	}
 	else if (r > 0 && cellTypeTop != 1 && cellTypeTop != 3 && dfs(p, r - 1, c, v)) { // move up
-		string s = "(" + to_string(r) + ',' + to_string(c) + ")";
 		v.push_back(s);
 		return true;
 	}
 	else if (r < 4 && cellTypeBottom != 1 && cellTypeBottom != 3 && dfs(p, r + 1, c, v)) { // move down
-		string s = "(" + to_string(r) + ',' + to_string(c) + ")";
 		v.push_back(s);
 		return true;
 	}
@@ -75,7 +73,7 @@ bool pathFinder::dfs(position *p, int r, int c,  vector<string>& v)
 	return false; // if the goal is not found using that path
 }
 
-pathFinder::pathFinder(cell* pmap, int initialRow, int initialColumn) // CR or RC?
+pathFinder::pathFinder(cell* pmap, int initialRow, int initialColumn)
 {	
 	map = pmap;
 	currentPosition.setColumn(initialColumn);
