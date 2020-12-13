2a7c8ceaccd410db7ee61deffa192e0b39297eb8
azharultambir <70976778+azharultambir@users.noreply.github.com>
2020-12-12 19:17:55 -0500

    pathFinder h final edit

--- ProjectAssignment/PathFinder.h
+++ ProjectAssignment/PathFinder.h
@@ -3,6 +3,7 @@
 #include "position.h"
 #include "cell.h"
 #include <vector>
+
 using namespace std;
 
 class pathFinder : public position
@@ -10,9 +11,9 @@ class pathFinder : public position
 private:
 	cell* map;
 	position currentPosition;
-	// void display();
+	void display(vector<string>);
 public:
 	void search();
-	bool dfs(position *p, int, int, vector<string>& v);
-	pathFinder(cell* pmap, int initialRow, int initialColumn); 
+	bool dfs(position*, int, int, vector<string>&);
+	pathFinder(cell*, int, int); 
 };
